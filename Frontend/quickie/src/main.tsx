753ca55731c180256bcbdf5a3c3d5fdd1dd480dd
Sairaj G. Adhav <sairaj10adhav@gmail.com>
2025-03-17 09:56:37 +0530

    Removed Analytics

--- Frontend/quickie/src/main.tsx
+++ Frontend/quickie/src/main.tsx
@@ -2,19 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { PostHogProvider } from "posthog-js/react";
+// import { PostHogProvider } from "posthog-js/react";
 
-const options = {
-  api_host: process.env.REACT_APP_PUBLIC_POSTHOG_HOST,
-};
+// const options = {
+//   api_host: process.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+// };
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <PostHogProvider
-      apiKey={process.env.REACT_APP_PUBLIC_POSTHOG_KEY as string}
-      options={options}
-    >
       <App />
-    </PostHogProvider>
   </StrictMode>
 );
